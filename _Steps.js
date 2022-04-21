9edd3dce8623836b4d10705d2abee942115c1ec3
Sajeda Akhter <sajedasaju85@gmail.com>
2022-04-22 03:05:15 +0600

    added steps

---
+++ _Steps.js
@@ -0,0 +1,29 @@
+/**
+ * -----------------------------------
+ * Server setup
+ * ------------------------------------
+ * 1. create a folder( menually or mkdir)
+ * 2. open command line to the folder
+ * 3.npm init
+ * 4. npm install express
+ * 5. create index.js
+ * ---------------------------
+ * For Express/Node server
+ * ----------------------------
+ * 1.Require express
+ * 2.create app variable
+ * 3.declare port
+ * 4.set app.get('/')
+ * 5.listen to port
+ * 6. node index.js
+ * 7.check your browser for that port
+ * 
+ * --------------------------------------
+ * Get setup
+ * --------------------------------------
+ * 1.cors middleware
+ * ---------------------------------------
+ * Post setup
+ * ----------------------------------------
+ * 1.
+ */
